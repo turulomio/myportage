ce0dc745d14e0851d4dfa8924e669299a05a8040
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2009-04-14 11:27:46 +0000

    No se genera planet.bin, sino que se descarga

--- app-misc/navit/files/navit-update-planet.sh
+++ app-misc/navit/files/navit-update-planet.sh
@@ -1,21 +1,3 @@
-#!/usr/bin/python
-import os, sys
-
-os.system("wget  http://planet.openstreetmap.org/planet-latest.osm.bz2.md5 -O /usr/local/share/osm/planet-latest.osm.bz2.md5 -o /dev/null")
-f=open("/usr/local/share/osm/planet-latest.osm.bz2.md5")
-md5remoto=f.read();
-f.close()
-
-o=os.popen("cd /usr/local/share/osm && md5sum planet-latest.osm.bz2")
-md5local=o.read()
-o.close()
-
-print md5remoto,  md5local
-
-if md5remoto==md5local:
-	print "Ya se ha descargado el fichero. Se sale del programa sin crear los mapas"
-	sys.exit(0)
-else:
-	os.popen("wget -c http://planet.openstreetmap.org/planet-latest.osm.bz2 -O planet-latest.osm.bz2")
-
-os.popen("bzcat /usr/local/share/osm/planet-latest.osm.bz2 | osm2navit /usr/share/navit/maps/planet.bin")
+#!/bin/bash
+wget -N http://maps.navit-project.org/planet.bin
+mv planet.bin /usr/share/navit/maps/planet.bin
