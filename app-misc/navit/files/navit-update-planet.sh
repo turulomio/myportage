1a480ad01c9d015b9c17b045cef0849bf9248bfa
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2009-12-25 11:25:45 +0000

    Sustituido por uno de internet

--- app-misc/navit/files/navit-update-planet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-my=`pwd`
-cd /usr/share/navit/maps/
-wget -N http://maps.navit-project.org/planet.bin
-cd ${my}
-echo '<map type="binfile" enabled="yes" data="$NAVIT_SHAREDIR/maps/planet.bin"/>' > /usr/share/navit/maps/planet.xml
-
