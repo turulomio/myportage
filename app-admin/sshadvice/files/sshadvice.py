81ff1773b46d6e932ca0d6bab473296496cac29a
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2011-12-19 13:39:25 +0000

    Primera versión y funciona

---
+++ app-admin/sshadvice/files/sshadvice.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+# Created by Pathogen @ www.hakhub.tk
+
+import datetime
+import subprocess as sp
+import re, os
+import pyinotify
+
+class MyEventHandler(pyinotify.ProcessEvent):
+	lastlog=datetime.datetime.now()
+	fallidos=0
+	validos=0
+	def process_IN_MODIFY(self, event):
+		cat = sp.Popen("cat /var/log/messages| grep -i sshd",stdout = sp.PIPE,shell=True)
+		for line in cat.stdout.readlines():
+			d=str(datetime.datetime.now().year)+" " + line[:-len(line)+15].decode('UTF-8')
+			if line.find(b'pam_unix(sshd:auth): authentication failure')!=-1:
+				try:
+					nueva=datetime.datetime.strptime(d, '%Y %b %d %H:%M:%S')
+				except:
+					print("error parsing date")
+				if self.lastlog<nueva:
+					self.fallidos=self.fallidos+1
+					os.system("espeak -v es 'Conexión remota fallida número {0}' >& /dev/null".format(self.fallidos))
+					self.lastlog=nueva
+					print (line[:-1])
+			elif line.find(b'pam_unix(sshd:session): session opened for user ')!=-1:
+				try:
+					nueva=datetime.datetime.strptime(d, '%Y %b %d %H:%M:%S')
+				except:
+					print("error parsing date")
+				if self.lastlog<nueva:
+					self.validos=self.validos+1
+					os.system("espeak -v es 'Conexión remota aceptada número {0}' >& /dev/null".format(self.validos))
+					self.lastlog=nueva
+					print (line[:-1])
+
+def main():
+
+	# watch manager
+	wm = pyinotify.WatchManager()
+	wm.add_watch('/var/log/messages', pyinotify.ALL_EVENTS, rec=True)
+
+	# event handler
+	eh = MyEventHandler()
+
+	# notifier
+	notifier = pyinotify.Notifier(wm, eh)
+	notifier.loop()
+
+
+main()
