c2d08ccaa5f571fa932a6c85165c01e937eb468f
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2014-06-12 07:25:46 +0000

    Database schema updated

--- media-video/pdffilms/files/pdffilms.sql
+++ media-video/pdffilms/files/pdffilms.sql
@@ -3,58 +3,38 @@
 --
 
 SET statement_timeout = 0;
+SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
 SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
 
-SET default_with_oids = true;
+SET default_with_oids = false;
 
+--
+-- Name: films; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
 
-CREATE TABLE peliculas (
-    id_peliculas integer DEFAULT nextval(('"peliculas_id_peliculas_seq"'::text)::regclass) NOT NULL,
-    fechagrabacion date,
-    titulo text,
-    lu_formatos integer DEFAULT 1,
-    sonidook boolean,
-    imagenok boolean,
-    cds integer DEFAULT 1,
-    comentario text,
+CREATE TABLE films (
+    id_films integer DEFAULT nextval(('"films_seq"'::text)::regclass) NOT NULL,
+    savedate date,
+    name text,
     foto oid,
-    tipofoto text DEFAULT 'jpg'::text,
-    funciona boolean DEFAULT true,
-    lu_situaciones integer,
-    lu_ficheros integer,
-    id_cds integer,
-    foto100 oid
+    id_dvd integer
 );
 
 
-ALTER TABLE public.peliculas OWNER TO postgres;
+ALTER TABLE public.films OWNER TO postgres;
 
 --
--- Name: peliculas_id_peliculas_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: films_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE peliculas_id_peliculas_seq
+CREATE SEQUENCE films_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -62,16 +42,27 @@ CREATE SEQUENCE peliculas_id_peliculas_seq
     CACHE 1;
 
 
-ALTER TABLE public.peliculas_id_peliculas_seq OWNER TO postgres;
+ALTER TABLE public.films_seq OWNER TO postgres;
+
+--
+-- Name: films_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY films
+    ADD CONSTRAINT films_pk PRIMARY KEY (id_films);
 
 
-ALTER TABLE ONLY peliculas
-    ADD CONSTRAINT peliculas_pkey PRIMARY KEY (id_peliculas);
-
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
 
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
 REVOKE ALL ON SCHEMA public FROM postgres;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
+
+--
+-- PostgreSQL database dump complete
+--
 
